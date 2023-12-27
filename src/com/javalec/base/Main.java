0ff61dcbee3ec2176b6dd3594cbdfca6a1426a01
DaegeunLee <foejakzm@naver.com>
2023-12-28 02:20:56 +0900

    add Main and Sign
    commit

--- src/com/javalec/base/Main.java
+++ src/com/javalec/base/Main.java
@@ -4,12 +4,39 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.ImageIcon;
+import java.awt.Color;
+import javax.swing.JTextField;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JRadioButton;
+import javax.swing.JTabbedPane;
+import javax.swing.ButtonGroup;
+import javax.swing.SwingConstants;
+import javax.swing.JPasswordField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JCheckBox;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Main {
 
 	private JFrame frame;
-	private JLabel lblNewLabel;
+	private JLabel lblLogo;
+	private JButton btnSign;
+	private JRadioButton rdbCust;
+	private JRadioButton rdbAdmin;
+	private JLabel lblNewLabel_1;
+	private JLabel lblNewLabel_1_1;
+	private JButton btnLogin;
+	private JTextField tfId;
+	private JPasswordField pfPassword;
+	private final ButtonGroup buttonGroup = new ButtonGroup();
+	private JCheckBox chbShow;
 
 	/**
 	 * Launch the application.
@@ -39,18 +66,189 @@ public class Main {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.getContentPane().setBackground(new Color(7, 7, 7));
 		frame.setTitle("로그인");
 		frame.setBounds(100, 100, 486, 489);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		frame.getContentPane().add(getLblNewLabel());
+		frame.getContentPane().add(getLblLogo());
+		frame.getContentPane().add(getBtnSign());
+		frame.getContentPane().add(getRdbCust());
+		frame.getContentPane().add(getRdbAdmin());
+		frame.getContentPane().add(getLblNewLabel_1());
+		frame.getContentPane().add(getLblNewLabel_1_1());
+		frame.getContentPane().add(getBtnLogin());
+		frame.getContentPane().add(getTfId());
+		frame.getContentPane().add(getPfPassword());
+		frame.getContentPane().add(getChbShow());
 	}
-	private JLabel getLblNewLabel() {
-		if (lblNewLabel == null) {
-			lblNewLabel = new JLabel("");
-			lblNewLabel.setIcon(new ImageIcon(Main.class.getResource("/com/javalec/images/로고.png")));
-			lblNewLabel.setBounds(115, 42, 236, 129);
+	private JLabel getLblLogo() {
+		if (lblLogo == null) {
+			lblLogo = new JLabel("");
+			lblLogo.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseClicked(MouseEvent e) {
+					if(e.getButton() == 1) {
+						homeScreen();
+					}
+				}
+			});
+			lblLogo.setIcon(new ImageIcon(Main.class.getResource("/com/javalec/images/로고.png")));
+			lblLogo.setBounds(124, 10, 236, 129);
 		}
-		return lblNewLabel;
+		return lblLogo;
 	}
-}
+	private JButton getBtnSign() {
+		if (btnSign == null) {
+			btnSign = new JButton("회원가입");
+			btnSign.setBounds(68, 364, 124, 53);
+		}
+		return btnSign;
+	}
+	private JRadioButton getRdbCust() {
+		if (rdbCust == null) {
+			rdbCust = new JRadioButton("회원");
+			rdbCust.setSelected(true);
+			buttonGroup.add(rdbCust);
+			rdbCust.setForeground(new Color(255, 255, 255));
+			rdbCust.setBackground(new Color(7, 7, 7));
+			rdbCust.setBounds(155, 145, 54, 23);
+		}
+		return rdbCust;
+	}
+	private JRadioButton getRdbAdmin() {
+		if (rdbAdmin == null) {
+			rdbAdmin = new JRadioButton("관리자");
+			buttonGroup.add(rdbAdmin);
+			rdbAdmin.setForeground(Color.WHITE);
+			rdbAdmin.setBackground(new Color(7, 7, 7));
+			rdbAdmin.setBounds(267, 145, 76, 23);
+		}
+		return rdbAdmin;
+	}
+	private JLabel getLblNewLabel_1() {
+		if (lblNewLabel_1 == null) {
+			lblNewLabel_1 = new JLabel("ID : ");
+			lblNewLabel_1.setHorizontalAlignment(SwingConstants.TRAILING);
+			lblNewLabel_1.setForeground(new Color(255, 255, 255));
+			lblNewLabel_1.setBounds(111, 193, 33, 34);
+		}
+		return lblNewLabel_1;
+	}
+	private JLabel getLblNewLabel_1_1() {
+		if (lblNewLabel_1_1 == null) {
+			lblNewLabel_1_1 = new JLabel("PW : ");
+			lblNewLabel_1_1.setHorizontalAlignment(SwingConstants.TRAILING);
+			lblNewLabel_1_1.setForeground(Color.WHITE);
+			lblNewLabel_1_1.setBounds(111, 249, 33, 34);
+		}
+		return lblNewLabel_1_1;
+	}
+	private JButton getBtnLogin() {
+		if (btnLogin == null) {
+			btnLogin = new JButton("로그인");
+			btnLogin.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					loginAction();
+				}
+			});
+			btnLogin.setBounds(275, 364, 124, 53);
+		}
+		return btnLogin;
+	}
+	private JTextField getTfId() {
+		if (tfId == null) {
+			tfId = new JTextField();
+			tfId.addKeyListener(new KeyAdapter() {
+				@Override
+				public void keyPressed(KeyEvent e) {
+					char specialKey = e.getKeyChar();
+					if(specialCharacter(specialKey)) {
+						JOptionPane.showMessageDialog(null, "ID에 특수문자는 안됩니다.", "경고", JOptionPane.ERROR_MESSAGE);
+						tfId.setText("");
+					}else if(e.getKeyCode() == KeyEvent.VK_ENTER){
+						pfPassword.requestFocus();
+					}
+				}
+			});
+			tfId.setBounds(163, 194, 184, 34);
+			tfId.setColumns(10);
+		}
+		return tfId;
+	}
+	private JPasswordField getPfPassword() {
+		if (pfPassword == null) {
+			pfPassword = new JPasswordField();
+			pfPassword.addKeyListener(new KeyAdapter() {
+				@Override
+				public void keyPressed(KeyEvent e) {
+					if(e.getKeyCode() == KeyEvent.VK_ENTER){
+						loginAction();
+					}
+				}
+			});
+			pfPassword.setBounds(163, 249, 184, 34);
+		}
+		return pfPassword;
+	}
+	private JCheckBox getChbShow() {
+		if (chbShow == null) {
+			chbShow = new JCheckBox("비밀번호 표시");
+			chbShow.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					checkPassword();
+				}
+			});
+			chbShow.setBackground(new Color(7, 7, 7));
+			chbShow.setForeground(new Color(255, 255, 255));
+			chbShow.setBounds(163, 304, 107, 23);
+		}
+		return chbShow;
+	}
+	
+	// --------- Functions ----------
+	private void checkPassword() {
+	    if (chbShow.isSelected()) {
+	        pfPassword.setEchoChar((char) 0); // 비밀번호 표시
+	    } else {
+	        pfPassword.setEchoChar('\u2022'); // 비밀번호 숨김
+	    }
+	}
+	
+	private void homeScreen() {
+		this.frame.setVisible(false);  // 현재화면 끄고
+		Main window = new Main();
+		window.frame.setVisible(true); // 홈 화면 키기
+	}
+	
+	private void loginAction() {
+		// 입력 안했을시 체크 받기
+		int check = inputCheck();
+		if(check != 0) {
+			JOptionPane.showMessageDialog(null, "항목을 입력 하세요.");
+		}
+		
+	}
+	
+	private int inputCheck() {
+		int checkResult = 0;
+		char[] passwordChars = pfPassword.getPassword();
+		
+		if(tfId.getText().trim().length() == 0) { // ID에 입력 안했을 때
+			checkResult++;
+			tfId.requestFocus();
+		}
+		if(passwordChars.length == 0) { // PW에 입력 안했을 때
+			checkResult++;
+			pfPassword.requestFocus();
+		}
+		
+		return checkResult;
+	}
+	
+	private boolean specialCharacter(char specialKey) {
+		// ID에 특수 문자 체크
+		return "!@#$%^&*()-_=+`~/?,.<>{}[];:|\"'\\".indexOf(specialKey) != -1;
+	}
+	
+} // End
---
+++ src/com/javalec/daegeun/Sign.java
@@ -0,0 +1,394 @@
+package com.javalec.daegeun;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+import com.javalec.base.Main;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class Sign extends JDialog {
+
+	private static final long serialVersionUID = 1L;
+	private JLabel lblNewLabel;
+	private JLabel lblNewLabel_1;
+	private JTextField tfId;
+	private JLabel lblNewLabel_1_1;
+	private JCheckBox chbShow;
+	private JLabel lblNewLabel_1_1_1;
+	private JPasswordField pfPassword1;
+	private JPasswordField pfPassword2;
+	private JLabel lblNewLabel_1_1_1_1;
+	private JTextField tfName;
+	private JLabel lblNewLabel_1_1_1_1_1;
+	private JTextField tfPhone;
+	private JLabel lblNewLabel_1_1_1_1_2;
+	private JTextField tfAddress;
+	private JButton btnSign;
+	private JButton btnCancel;
+	private JButton btnCheck;
+	private JButton btnIdSame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Sign dialog = new Sign();
+					dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+					dialog.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public Sign() {
+		getContentPane().setBackground(new Color(7, 7, 7));
+		setTitle("회원가입");
+		setBounds(100, 100, 502, 514);
+		getContentPane().setLayout(null);
+		getContentPane().add(getLblNewLabel());
+		getContentPane().add(getLblNewLabel_1());
+		getContentPane().add(getTfId());
+		getContentPane().add(getLblNewLabel_1_1());
+		getContentPane().add(getChbShow());
+		getContentPane().add(getLblNewLabel_1_1_1());
+		getContentPane().add(getPfPassword1());
+		getContentPane().add(getPfPassword2());
+		getContentPane().add(getLblNewLabel_1_1_1_1());
+		getContentPane().add(getTfName());
+		getContentPane().add(getLblNewLabel_1_1_1_1_1());
+		getContentPane().add(getTfPhone());
+		getContentPane().add(getLblNewLabel_1_1_1_1_2());
+		getContentPane().add(getTfAddress());
+		getContentPane().add(getBtnSign());
+		getContentPane().add(getBtnCancel());
+		getContentPane().add(getBtnCheck());
+		getContentPane().add(getBtnIdSame());
+
+	}
+
+	private JLabel getLblNewLabel() {
+		if (lblNewLabel == null) {
+			lblNewLabel = new JLabel("");
+			lblNewLabel.setIcon(new ImageIcon(Sign.class.getResource("/com/javalec/images/로고.png")));
+			lblNewLabel.setBounds(122, 10, 251, 106);
+		}
+		return lblNewLabel;
+	}
+	private JLabel getLblNewLabel_1() {
+		if (lblNewLabel_1 == null) {
+			lblNewLabel_1 = new JLabel("ID :");
+			lblNewLabel_1.setHorizontalAlignment(SwingConstants.TRAILING);
+			lblNewLabel_1.setForeground(new Color(255, 255, 255));
+			lblNewLabel_1.setBounds(92, 130, 39, 15);
+		}
+		return lblNewLabel_1;
+	}
+	private JTextField getTfId() {
+		if (tfId == null) {
+			tfId = new JTextField();
+			tfId.addKeyListener(new KeyAdapter() {
+				@Override
+				public void keyPressed(KeyEvent e) {
+					char specialKey = e.getKeyChar();
+					if(specialCharacter(specialKey)) {
+						JOptionPane.showMessageDialog(null, "ID에 특수문자는 안됩니다.", "경고", JOptionPane.ERROR_MESSAGE);
+						tfId.setText("");
+					}else if(e.getKeyCode() == KeyEvent.VK_ENTER){
+						pfPassword1.requestFocus();
+					}
+				}
+			});
+			tfId.setBounds(164, 126, 148, 23);
+			tfId.setColumns(10);
+		}
+		return tfId;
+	}
+	private JLabel getLblNewLabel_1_1() {
+		if (lblNewLabel_1_1 == null) {
+			lblNewLabel_1_1 = new JLabel("PW :");
+			lblNewLabel_1_1.setHorizontalAlignment(SwingConstants.TRAILING);
+			lblNewLabel_1_1.setForeground(Color.WHITE);
+			lblNewLabel_1_1.setBounds(92, 173, 39, 15);
+		}
+		return lblNewLabel_1_1;
+	}
+	private JCheckBox getChbShow() {
+		if (chbShow == null) {
+			chbShow = new JCheckBox("비밀번호 표시");
+			chbShow.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					checkPassword();
+				}
+			});
+			chbShow.setForeground(Color.WHITE);
+			chbShow.setBackground(new Color(7, 7, 7));
+			chbShow.setBounds(159, 199, 107, 23);
+		}
+		return chbShow;
+	}
+	private JLabel getLblNewLabel_1_1_1() {
+		if (lblNewLabel_1_1_1 == null) {
+			lblNewLabel_1_1_1 = new JLabel("PW Check : ");
+			lblNewLabel_1_1_1.setHorizontalAlignment(SwingConstants.TRAILING);
+			lblNewLabel_1_1_1.setForeground(Color.WHITE);
+			lblNewLabel_1_1_1.setBounds(62, 237, 69, 15);
+		}
+		return lblNewLabel_1_1_1;
+	}
+	private JPasswordField getPfPassword1() {
+		if (pfPassword1 == null) {
+			pfPassword1 = new JPasswordField();
+			pfPassword1.addKeyListener(new KeyAdapter() {
+				@Override
+				public void keyPressed(KeyEvent e) {
+					if(e.getKeyCode() == KeyEvent.VK_ENTER){
+						pfPassword2.requestFocus();
+					}
+				}
+			});
+			pfPassword1.setBounds(164, 169, 148, 23);
+		}
+		return pfPassword1;
+	}
+	private JPasswordField getPfPassword2() {
+		if (pfPassword2 == null) {
+			pfPassword2 = new JPasswordField();
+			pfPassword2.addKeyListener(new KeyAdapter() {
+				@Override
+				public void keyPressed(KeyEvent e) {
+					if(e.getKeyCode() == KeyEvent.VK_ENTER){
+						tfName.requestFocus();
+					}
+				}
+			});
+			pfPassword2.setBounds(164, 233, 148, 23);
+		}
+		return pfPassword2;
+	}
+	private JLabel getLblNewLabel_1_1_1_1() {
+		if (lblNewLabel_1_1_1_1 == null) {
+			lblNewLabel_1_1_1_1 = new JLabel("성명 :");
+			lblNewLabel_1_1_1_1.setHorizontalAlignment(SwingConstants.TRAILING);
+			lblNewLabel_1_1_1_1.setForeground(Color.WHITE);
+			lblNewLabel_1_1_1_1.setBounds(81, 279, 50, 15);
+		}
+		return lblNewLabel_1_1_1_1;
+	}
+	private JTextField getTfName() {
+		if (tfName == null) {
+			tfName = new JTextField();
+			tfName.addKeyListener(new KeyAdapter() {
+				@Override
+				public void keyPressed(KeyEvent e) {
+					// TextField에 숫자가 입력 되면 지운다
+					char specialKey = e.getKeyChar();
+					if(e.getKeyChar() >= '0' && e.getKeyChar() <= '9' || e.getKeyCode() == KeyEvent.VK_SPACE || specialCharacter(specialKey)) {
+						JOptionPane.showMessageDialog(null, "이름칸에 글자만 입력하세요", "경고", JOptionPane.ERROR_MESSAGE);
+						tfName.setText("");
+						// Enter키로 다음 입력칸 이동!!
+					}else if(e.getKeyCode() == KeyEvent.VK_ENTER){
+						tfPhone.requestFocus();
+					}else {
+						
+					}
+				}
+			});
+			tfName.setColumns(10);
+			tfName.setBounds(164, 275, 148, 23);
+		}
+		return tfName;
+	}
+	private JLabel getLblNewLabel_1_1_1_1_1() {
+		if (lblNewLabel_1_1_1_1_1 == null) {
+			lblNewLabel_1_1_1_1_1 = new JLabel("전화번호 :");
+			lblNewLabel_1_1_1_1_1.setHorizontalAlignment(SwingConstants.TRAILING);
+			lblNewLabel_1_1_1_1_1.setForeground(Color.WHITE);
+			lblNewLabel_1_1_1_1_1.setBounds(62, 324, 69, 15);
+		}
+		return lblNewLabel_1_1_1_1_1;
+	}
+	private JTextField getTfPhone() {
+		if (tfPhone == null) {
+			tfPhone = new JTextField();
+			tfPhone.addKeyListener(new KeyAdapter() {
+				@Override
+				public void keyPressed(KeyEvent e) {
+					// TextField에 숫자가 입력 되게한다
+					if(e.getKeyChar() >= '0' && e.getKeyChar() <= '9'|| e.getKeyCode() == KeyEvent.VK_BACK_SPACE || e.getKeyCode() == KeyEvent.VK_MINUS) {
+						// Enter키로 다음 입력칸 이동!!
+					}else if(e.getKeyCode() == KeyEvent.VK_ENTER){
+						tfAddress.requestFocus();
+						// 글자 잘못 입력시 지우기
+					}else {
+						JOptionPane.showMessageDialog(null, "010-0000-0000 형태로 입력하세요.", "경고", JOptionPane.ERROR_MESSAGE);
+						tfAddress.setText("");
+					}
+				}
+			});
+			tfPhone.setColumns(10);
+			tfPhone.setBounds(164, 320, 251, 23);
+		}
+		return tfPhone;
+	}
+	private JLabel getLblNewLabel_1_1_1_1_2() {
+		if (lblNewLabel_1_1_1_1_2 == null) {
+			lblNewLabel_1_1_1_1_2 = new JLabel("주소 :");
+			lblNewLabel_1_1_1_1_2.setHorizontalAlignment(SwingConstants.TRAILING);
+			lblNewLabel_1_1_1_1_2.setForeground(Color.WHITE);
+			lblNewLabel_1_1_1_1_2.setBounds(81, 369, 50, 15);
+		}
+		return lblNewLabel_1_1_1_1_2;
+	}
+	private JTextField getTfAddress() {
+		if (tfAddress == null) {
+			tfAddress = new JTextField();
+			tfAddress.addKeyListener(new KeyAdapter() {
+				@Override
+				public void keyPressed(KeyEvent e) {
+					char specialKey = e.getKeyChar();
+					if(specialCharacter(specialKey)) {
+						JOptionPane.showMessageDialog(null, "특수문자는 안됩니다.", "경고", JOptionPane.ERROR_MESSAGE);
+						tfAddress.setText("");
+					}else if(e.getKeyCode() == KeyEvent.VK_ENTER){
+						// 아직 작성 안함
+					}
+				}
+			});
+			tfAddress.setColumns(10);
+			tfAddress.setBounds(164, 365, 251, 23);
+		}
+		return tfAddress;
+	}
+	private JButton getBtnSign() {
+		if (btnSign == null) {
+			btnSign = new JButton("회원가입");
+			btnSign.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+				}
+			});
+			btnSign.setBounds(81, 416, 124, 53);
+		}
+		return btnSign;
+	}
+	private JButton getBtnCancel() {
+		if (btnCancel == null) {
+			btnCancel = new JButton("취소");
+			btnCancel.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					homeScreen();
+				}
+			});
+			btnCancel.setBounds(288, 416, 124, 53);
+		}
+		return btnCancel;
+	}
+	private JButton getBtnCheck() {
+		if (btnCheck == null) {
+			btnCheck = new JButton("확인");
+			btnCheck.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					comparePassword();
+				}
+			});
+			btnCheck.setBounds(322, 232, 93, 23);
+		}
+		return btnCheck;
+	}
+	
+	// --- function ---
+	private void checkPassword() {
+	    if (chbShow.isSelected()) {
+	        pfPassword1.setEchoChar((char) 0); // 비밀번호 표시
+	    } else {
+	        pfPassword1.setEchoChar('\u2022'); // 비밀번호 숨김
+	    }
+	}
+	private void comparePassword() {
+		char[] pass1 = pfPassword1.getPassword();
+		String passString1 = new String(pass1);
+		
+		char[] pass2 = pfPassword2.getPassword();
+		String passString2 = new String(pass2);
+		
+		if(passString1.equals("") && passString2.equals("")) {
+			JOptionPane.showMessageDialog(null, "비밀번호가 입력되지 않았습니다. 다시 입력하세요.");
+		}else if(passString1.equals(passString2)){
+			JOptionPane.showMessageDialog(null, "비밀번호가 일치합니다.");
+		}else {
+			JOptionPane.showMessageDialog(null, "비밀번호가 일치하지 않습니다. 다시 입력하세요.");
+		}
+	}
+	
+	private void homeScreen() {
+		// 첫 화면으로 돌아가기
+		Main window = new Main();
+		window.main(null);
+		
+		this.setVisible(false);
+	}
+	
+	// 작성 안했을시 처리
+	private int inputCheck() {
+		int checkResult = 0;
+		char[] passwordChars = pfPassword1.getPassword();
+		
+		if(tfId.getText().trim().length() == 0) { // ID에 입력 안했을 때
+			checkResult++;
+			tfId.requestFocus();
+		}
+		if(passwordChars.length == 0) { // PW에 입력 안했을 때
+			checkResult++;
+			pfPassword1.requestFocus();
+		}
+		if(tfName.getText().trim().length() == 0) {
+			checkResult++;
+			tfName.requestFocus();
+		}
+		if(tfPhone.getText().trim().length() == 0) {
+			checkResult++;
+			tfPhone.requestFocus();
+		}
+		if(tfAddress.getText().trim().length() == 0) {
+			checkResult++;
+			tfAddress.requestFocus();
+		}
+		
+		return checkResult;
+	}
+	
+	private boolean specialCharacter(char specialKey) {
+		// 특수 문자 확인을 위한 조건 추가
+		return "!@#$%^&*()-_=+`~/?,.<>{}[];:|\"'\\".indexOf(specialKey) != -1;
+	}
+	
+	
+	
+	private JButton getBtnIdSame() {
+		if (btnIdSame == null) {
+			btnIdSame = new JButton("중복 확인");
+			btnIdSame.setBounds(322, 126, 93, 23);
+		}
+		return btnIdSame;
+	}
+} // End
