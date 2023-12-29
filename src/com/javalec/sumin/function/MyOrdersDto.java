2ff00d6f9bf63ba65b5ef12f310b8e11eda90c56
Sumin Kim <411sumin@gmail.com>
2023-12-29 21:37:01 +0900

    yes

--- src/com/javalec/sumin/function/MyOrdersDto.java
+++ src/com/javalec/sumin/function/MyOrdersDto.java
@@ -6,80 +6,139 @@ public class MyOrdersDto {
 	
 	//Field
 	
-	int stgseqno;
+	int purnum;
+	String custid;
 	String stomodelnum;
-	int stgsize;
-	int stgqty;
-	String stgdate; 
-
+	int purqty;
+	int purprice;
+	String purdate;
 	
 	
 	
 	
-
 	
-	
-	
-
-	public MyOrdersDto(int stgseqno, String stomodelnum, int stgsize, int stgqty, String stgdate,
-			FileInputStream file2) {
+	public MyOrdersDto(int purnum, String custid, String stomodelnum, int purqty, int purprice, String purdate) {
 		super();
-		this.stgseqno = stgseqno;
+		this.purnum = purnum;
+		this.custid = custid;
 		this.stomodelnum = stomodelnum;
-		this.stgsize = stgsize;
-		this.stgqty = stgqty;
-		this.stgdate = stgdate;
-	
+		this.purqty = purqty;
+		this.purprice = purprice;
+		this.purdate = purdate;
 	}
 
-	public MyOrdersDto(int stgseqno, String stomodelnum, int stgsize, int stgqty, String stgdate) {
-		super();
-		this.stgseqno = stgseqno;
-		this.stomodelnum = stomodelnum;
-		this.stgsize = stgsize;
-		this.stgqty = stgqty;
-		this.stgdate = stgdate;
+
+
+
+
+	public int getPurnum() {
+		return purnum;
 	}
 
-	public int getStgseqno() {
-		return stgseqno;
+
+
+
+
+	public void setPurnum(int purnum) {
+		this.purnum = purnum;
 	}
 
-	public void setStgseqno(int stgseqno) {
-		this.stgseqno = stgseqno;
+
+
+
+
+	public String getCustid() {
+		return custid;
 	}
+
+
+
+
+
+	public void setCustid(String custid) {
+		this.custid = custid;
+	}
+
+
+
+
 
 	public String getStomodelnum() {
 		return stomodelnum;
 	}
 
+
+
+
+
 	public void setStomodelnum(String stomodelnum) {
 		this.stomodelnum = stomodelnum;
 	}
 
-	public int getStgsize() {
-		return stgsize;
+
+
+
+
+	public int getPurqty() {
+		return purqty;
 	}
 
-	public void setStgsize(int stgsize) {
-		this.stgsize = stgsize;
+
+
+
+
+	public void setPurqty(int purqty) {
+		this.purqty = purqty;
 	}
 
-	public int getStgqty() {
-		return stgqty;
+
+
+
+
+	public int getPurprice() {
+		return purprice;
 	}
 
-	public void setStgqty(int stgqty) {
-		this.stgqty = stgqty;
+
+
+
+
+	public void setPurprice(int purprice) {
+		this.purprice = purprice;
 	}
 
-	public String getStgdate() {
-		return stgdate;
+
+
+
+
+	public String getPurdate() {
+		return purdate;
 	}
 
-	public void setStgdate(String stgdate) {
-		this.stgdate = stgdate;
+
+
+
+
+	public void setPurdate(String purdate) {
+		this.purdate = purdate;
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
